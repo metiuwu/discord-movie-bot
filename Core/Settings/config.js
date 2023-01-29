c33edb0b990e52007545e1acc7cbff343334874f
metiuwu <110869588+metiuwu@users.noreply.github.com>
2023-01-29 20:54:55 +0300

    Update config.js

--- Core/Settings/config.js
+++ Core/Settings/config.js
@@ -1,11 +1,7 @@
 module.exports = {
 
     token: "",
-    prefix: "!", // işlevsiz
-
-    ownerID: [],
-
-    mongoURI: "mongodb+srv://metiuwmoviedb:sK9rXCGPGRSZzquh@cluster0.qewbql0.mongodb.net/?retryWrites=true&w=majority",  // işlevsiz
+    mongoURI: "",
 
 }
 
